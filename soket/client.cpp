d11ce89a1f924d2ec2daa9b5b3910be7bb79c29b
woojin2017 <76552988+woojin2017@users.noreply.github.com>
2022-08-23 17:26:07 +0900

    Update client.cpp

--- soket/client.cpp
+++ soket/client.cpp
@@ -6,39 +6,61 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
+#define BUF_SIZE 1024
 void error_handling(const char *message);
 
-int main(int argc, char* argv[])
+int main(int argc, const char *argv[])
 {
-    if(argc!=3){
+    int sock;
+    char message[BUF_SIZE];
+    int str_len, recv_len, recv_cnt;
+    struct sockaddr_in serv_adr;
+
+    if(argc!=3) {
         printf("Usage : %s <IP> <port>\n", argv[0]);
         exit(1);
     }
 	
-    int sock=socket(PF_INET, SOCK_STREAM, 0);
-    if(sock == -1)
+    sock=socket(PF_INET, SOCK_STREAM, 0);   
+    if(sock==-1)
         error_handling("socket() error");
+    
+    memset(&serv_adr, 0, sizeof(serv_adr));
+    serv_adr.sin_family=AF_INET;
+    serv_adr.sin_addr.s_addr=inet_addr(argv[1]);
+    serv_adr.sin_port=htons(atoi(argv[2]));
 	
-    // 클라이언트와 마찬가지로 주소정보를 초기화
-    struct sockaddr_in serv_addr;
-    memset(&serv_addr, 0, sizeof(serv_addr));
-    serv_addr.sin_family=AF_INET;
-    serv_addr.sin_addr.s_addr=inet_addr(argv[1]);
-    serv_addr.sin_port=htons(atoi(argv[2]));
-		
-    // 서버의 주소정보로 클라이언트 소켓이 연결요청을 한다.
-    if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))==-1) 
+    if(connect(sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr))==-1)
         error_handling("connect() error!");
+    else
+        puts("Connected...........");
 	
-    // 성공적으로 서버에게서 메시지를 받아오면
-    // 0을 반환하고 실패하면 -1을 반환한다.
-    char message[30];
-    int str_len;
-    str_len=read(sock, message, sizeof(message)-1);
-    if(str_len==-1)
-        error_handling("read() error!");
+    // 사용자가 q를 입력하기 전까지 계속 반복
+    while(1) 
+    {
+        fputs("Input message(Q to quit): ", stdout);
+        fgets(message, BUF_SIZE, stdin);
+		
+        if(!strcmp(message,"q\n") || !strcmp(message,"Q\n"))
+            break;
+
+        // 서버에게 데이터를 한번 보낸다.
+        str_len=write(sock, message, strlen(message));
+		
+        recv_len=0;
+        // 서버로부터 전달되는 데이터를 1바이트씩 message변수에 저장한다.
+        while(recv_len<str_len)
+        {
+            recv_cnt=read(sock, &message[recv_len], BUF_SIZE-1);
+            if(recv_cnt==-1)
+                error_handling("read() error!");
+            recv_len+=recv_cnt;
+        }
+		
+        message[recv_len]=0;
+        printf("Message from server: %s", message);
+    }
 	
-    printf("Message from server: %s \n", message);  
     close(sock);
     return 0;
 }
