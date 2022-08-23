767d123407d413a507449bb3a5351453e7bc822f
woojin2017 <76552988+woojin2017@users.noreply.github.com>
2022-08-23 17:30:04 +0900

    Update server.cpp

--- soket/server.cpp
+++ soket/server.cpp
@@ -3,59 +3,70 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <iostream>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
-void error_handling(const char *message);
+#define BUF_SIZE 1024
+void error_handling(char *message);
 
 int main(int argc, char *argv[])
 {
-    struct sockaddr_in clnt_addr; 
-    
-    if(argc!=2){
+    int serv_sock, clnt_sock;
+    char message[BUF_SIZE];
+    int str_len, i;
+	
+    struct sockaddr_in serv_adr;
+    struct sockaddr_in clnt_adr;
+    socklen_t clnt_adr_sz;
+	
+    if(argc!=2) {
         printf("Usage : %s <port>\n", argv[0]);
         exit(1);
     }
 	
-    // 소켓을 생성
-    int serv_sock=socket(PF_INET, SOCK_STREAM, 0);
-    if(serv_sock == -1)
+    serv_sock=socket(PF_INET, SOCK_STREAM, 0);   
+    if(serv_sock==-1)
         error_handling("socket() error");
 	
-    // 주소 정보 초기화
-    struct sockaddr_in serv_addr;
-    memset(&serv_addr, 0, sizeof(serv_addr));
-    serv_addr.sin_family=AF_INET;
-    // INADDR_ANY는 서버 자신의 IP주소
-    serv_addr.sin_addr.s_addr=htonl(INADDR_ANY);
-    serv_addr.sin_port=htons(atoi(argv[1]));
+    memset(&serv_adr, 0, sizeof(serv_adr));
+    serv_adr.sin_family=AF_INET;
+    serv_adr.sin_addr.s_addr=htonl(INADDR_ANY);
+    serv_adr.sin_port=htons(atoi(argv[1]));
+
+    if(bind(serv_sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr))==-1)
+        error_handling("bind() error");
 	
-    // 소켓에 위에서 생성한 주소 정보를 할당
-    if(bind(serv_sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr))==-1 )
-        error_handling("bind() error"); 
-	
-    // 클라이언트가 연결요청이 가능하도록 5크기의 대기실 생성
     if(listen(serv_sock, 5)==-1)
         error_handling("listen() error");
 	
-    // accept()함수호출을 해서 실제 데이터를 보낼 수 있는 소켓을 생성
-    socklen_t clnt_addr_size=sizeof(clnt_addr);
-    int clnt_sock=accept(serv_sock, (struct sockaddr*)&clnt_addr,&clnt_addr_size);
-    if(clnt_sock==-1)
-        error_handling("accept() error");  
-    
-    // 메시지를 보낸다
-    char message[]="Hello World!";
-    write(clnt_sock, message, sizeof(message));
+    clnt_adr_sz=sizeof(clnt_adr);
+	
+    // 여러 클라이언트와의 통신을 위해서 반복문 추가
+    for(i=0; i<5; i++)
+    {
+        // 서버와 각 클라이언트와의 통신을 위해서 생성된 소켓
+        clnt_sock=accept(serv_sock, (struct sockaddr*)&clnt_adr, &clnt_adr_sz);
+        if(clnt_sock==-1)
+            error_handling("accept() error");
+        else
+            printf("Connected client %d \n", i+1);
+		
+        // 클라이언트로부터 전송된 데이터를 수신한다.
+        // 그리고 다시 클라이언트에게 받았던 데이터를 송신한다.
+        while((str_len=read(clnt_sock, message, BUF_SIZE))!=0) {
+            write(clnt_sock, message, str_len);
+            std::cout << message << std::endl;
+        }
 
-    
-    // 서버, 클라이언트 소켓의 연결을 해제
-    close(clnt_sock);	
+        close(clnt_sock);
+    }
+
     close(serv_sock);
     return 0;
 }
 
-void error_handling(const char *message)
+void error_handling(char *message)
 {
     fputs(message, stderr);
     fputc('\n', stderr);
